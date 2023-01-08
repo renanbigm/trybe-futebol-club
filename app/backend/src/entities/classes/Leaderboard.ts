ed2ec8cef4fb5f8c9db11f2ef45c59c186787bc4
Renan Fagundes <renan@gmail.com>
2023-01-08 01:04:41 -0300

    req 29 completoooooooooooooooooooooooooooo \o/

--- app/backend/src/entities/classes/Leaderboard.ts
+++ app/backend/src/entities/classes/Leaderboard.ts
@@ -4,58 +4,43 @@ import MatcheService from '../../services/MatcheService';
 import ILeaderBoard from '../interfaces/ILeaderboard';
 import IFullMatche from '../interfaces/IFullMatche';
 import IResumeMatches from '../interfaces/IResumeMatches';
+import TeamService from '../../services/TeamService';
 
 class Leaderboard {
   static async getBoard() {
     const allMatches = await Leaderboard.getTeamsMatches();
 
-    // const result = allMatches.reduce((acc, curr) => {
-    //   console.log(acc);
-    //   curr.map((el) => acc.push({
-    //     id: el.homeTeam,
-    //     name: el.teamHome.teamName,
-    //     goalsFavor: el.homeTeamGoals,
-    //     goalsOwn: el.awayTeamGoals,
-    //   }));
-    //   // console.log(curr);
-    //   return acc.push(curr);
-    // }, []);
-    return allMatches;
+    const sortLeaderboard = allMatches.sort((a: any, b: any) =>
+      (
+        b.totalPoints - a.totalPoints
+      || b.totalVictories - a.totalVictories
+      || b.goalsBalance - a.goalsBalance
+      || b.goalsFavor - a.goalsFavor
+      || b.goalsOwn - a.goalsOwn
+      ));
+
+    return sortLeaderboard;
   }
 
   static async getTeamsMatches() {
     const allMatches = await MatcheService.getFilteredMatches(false) as unknown as IFullMatche[];
+    const allTeams = await TeamService.getAll();
     const resume = allMatches.map((el: IFullMatche) => Leaderboard.matchesResume(el));
-    console.log(resume);
 
-    const getTeamsResume = resume.map((matche) => {
-      console.log(matche.homeTeamName);
-      const homeTeams = Leaderboard.handleHomeTeams(matche, resume);
-      return homeTeams;
-    });
+    const getTeamsResume = resume.map((matche) => Leaderboard.handleHomeTeams(matche, resume));
 
-    const teamsResume: any = [...getTeamsResume];
+    const result = allTeams.map((el: any) =>
+      getTeamsResume.find((target) => target.name === el.teamName));
 
-    return teamsResume;
+    return result;
   }
 
-  //     "matcheId": 1,
-  //     "homeTeamId": 16,
-  //     "homeTeamname": "São Paulo",
-  //     "awayTeamId": 8,
-  //     "awayTeamname": "Grêmio",
-  //     "goalsFavor": 3,
-  //     "goalsOwn": 1,
-  //     "result": "homeWins"
-  //   },
-
   static handleHomeTeams(matche: any, resume: any) {
-    const totalMatches = resume.filter((el: any) => matche.homeTeamName === el.homeTeamName
-      || matche.homeTeamName === el.awayTeamName);
+    const totalMatches = resume.filter((el: any) => matche.homeTeamName === el.homeTeamName);
+    // || matche.homeTeamName === el.awayTeamName
 
-    // console.log('totalTeamsMatches: ', totalMatches.length, 'resumes: ', matche.homeTeamName);
     const stats = Leaderboard.buildLeaderboard(totalMatches, matche);
-    // console.log(matche.homeTeamName, stats);
+
     const totalPoints = (stats.totalWins * 3) + stats.totalDraws;
     const teamData = {
       name: matche.homeTeamName,
@@ -74,23 +59,23 @@ class Leaderboard {
 
   static buildLeaderboard(totalMatches: any, matche: any) {
     const teamStatus = { totalWins: 0, totalLosses: 0, goalsFavor: 0, goalsOwn: 0, totalDraws: 0 };
-
     totalMatches.forEach((el: any) => {
-      if (matche.homeTeamName === el.homeTeamName) {
-        teamStatus.goalsFavor += el.homeTeamGoals;
-        teamStatus.goalsOwn += el.awayTeamGoals;
-        if (el.result === 'homeWins') teamStatus.totalWins += 1;
-        else if (el.result === 'draw') teamStatus.totalDraws += 1;
-        else teamStatus.totalLosses += 1;
-      } else if (matche.homeTeamName === el.awayTeamName) {
-        teamStatus.goalsFavor += el.awayTeamGoals;
-        teamStatus.goalsOwn += el.homeTeamGoals;
-        if (el.result === 'homeWins') teamStatus.totalWins += 1;
-        else if (el.result === 'draw') teamStatus.totalDraws += 1;
-        else teamStatus.totalLosses += 1;
+      if (param === home) {
+        if (matche.homeTeamName === el.homeTeamName) {
+          teamStatus.goalsFavor += el.homeTeamGoals;
+          teamStatus.goalsOwn += el.awayTeamGoals;
+          if (el.result === 'homeWins') teamStatus.totalWins += 1;
+          else if (el.result === 'awayWins') teamStatus.totalLosses += 1;
+          else teamStatus.totalDraws += 1;
+        } else if (matche.homeTeamName === el.awayTeamName) {
+          teamStatus.goalsFavor += el.awayTeamGoals;
+          teamStatus.goalsOwn += el.homeTeamGoals;
+          if (el.result === 'awayWins') teamStatus.totalWins += 1;
+          else if (el.result === 'homeWins') teamStatus.totalLosses += 1;
+          else teamStatus.totalDraws += 1;
+        }
       }
     });
-
     return teamStatus;
   }
 
@@ -117,40 +102,3 @@ class Leaderboard {
 }
 
 export default Leaderboard;
-
-// name, totalPoints, totalGames, totalVictories, totalDraws,
-// totalLosses, goalsFavor, goalsOwn, goalsBalance, efficiency
-// [
-//   {
-//     "id": 1,
-//     "homeTeam": 16,
-//     "homeTeamGoals": 3,
-//     "awayTeam": 8,
-//     "awayTeamGoals": 1,
-//     "inProgress": false,
-//     "teamHome": {
-//       "id": 16,
-//       "teamName": "São Paulo"
-//     },
-//     "teamAway": {
-//       "id": 8,
-//       "teamName": "Grêmio"
-//     }
-//   },
-// ]
-// {
-
-// // if (match.homeTeam === curr) {
-//   acc.push({
-//     name: match.teamHome.teamName,
-//     totalPoints: 1,
-//     totalGames: 1,
-//     totalVictories: 1,
-//     totalDraws: 1,
-//     totalLosses: 1,
-//     goalsFavor: 1,
-//     goalsOwn: 1,
-//     goalsBalance: 1,
-//     efficiency: '1',
-//   });
-// }
